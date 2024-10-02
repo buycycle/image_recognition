3574471810a016ae622a72fde12e4330a84b9432
rauner <29773634+rauner@users.noreply.github.com>
2024-10-02 10:21:58 +0200

    handle exceptions

--- cdk/lambda/index.py
+++ cdk/lambda/index.py
@@ -1,7 +1,7 @@
-import os
 import json
 import boto3
 import base64
+import os
 from google.cloud import vision
 from botocore.exceptions import ClientError
 from model import get_google_vision_client, process_image
@@ -18,23 +18,32 @@ def get_secret(secret_name):
         print(f"Error retrieving secret: {e}")
         raise e
 def lambda_handler(event, context):
-    # Get the bucket and object key from the S3 event
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
-    # Get the image from S3
-    response = s3_client.get_object(Bucket=bucket, Key=key)
-    image_data = response['Body'].read()
-    # Retrieve Google API credentials from Secrets Manager
-    secret_name = os.environ['SECRET_NAME']
-    secret = get_secret(secret_name)
-    google_api_key = secret['IMAGE_RECOGNITION_GOOGLE_API_KEY']
-    google_project_name = secret['IMAGE_RECOGNITION_GOOGLE_PROJECT_NAME']
-    # Initialize the Google Vision client with the API key
-    #vision_client = get_google_vision_client(google_api_key)
-    # Process the image using the model logic
-    #most_likely_family_id = process_image(image_data, vision_client)
-    return {
-        'statusCode': 200,
-        'body': json.dumps({'most_likely_family_id': "test"})
-    }
+    try:
+        # Log the incoming event
+        print("Received event: " + json.dumps(event, indent=2))
+        # Get the bucket and object key from the S3 event
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = event['Records'][0]['s3']['object']['key']
+        # Get the image from S3
+        response = s3_client.get_object(Bucket=bucket, Key=key)
+        image_data = response['Body'].read()
+        # Retrieve Google API credentials from Secrets Manager
+        secret_name = os.environ['SECRET_NAME']
+        secret = get_secret(secret_name)
+        google_api_key = secret['IMAGE_RECOGNITION_GOOGLE_API_KEY']
+        google_project_name = secret['IMAGE_RECOGNITION_GOOGLE_PROJECT_NAME']
+        # Initialize the Google Vision client with the API key
+        # vision_client = get_google_vision_client(google_api_key)
+        # Process the image using the model logic
+        # most_likely_family_id = process_image(image_data, vision_client)
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'most_likely_family_id': "test"})
+        }
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'message': 'Internal server error'})
+        }
 
